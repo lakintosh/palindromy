a40cb9d3b78227e4d520c1f1b1320341161af4fe
Krystian Lakatosz <lakatosz.krystian@outlook.com>
2021-11-25 15:04:46 +0100

    Call function 3 times to test

--- palindromy.py
+++ palindromy.py
@@ -4,3 +4,8 @@ def check_palindrome(word):
     if word == reversed_word:
         return True
     else: return False
+
+
+print(check_palindrome("Krystian"))
+print(check_palindrome("Zakaz"))
+print(check_palindrome("kajak"))

958728b7e4c6580163ad32e42cb11eaea38ca7ee
Krystian Lakatosz <lakatosz.krystian@outlook.com>
2021-11-25 15:22:15 +0100

    Add check if variable is a string

--- palindromy.py
+++ palindromy.py
@@ -1,17 +1,20 @@
 def check_palindrome(word):
     """
     Checks if word and reversed word are equal.
-    Argument should be a string. 
+    Argument should be a string.
     :param word:
     :returns: True or False
     """
-    word = word.lower()
-    reversed_word = word[::-1]
-    if word == reversed_word:
-        return True
-    else: return False
+    if type(word) == str:
+        word = word.lower()
+        reversed_word = word[::-1]
+        if word == reversed_word:
+            return True
+        else: return False
+    else: return "Argument should be a string!"
 
 
 print(check_palindrome("Krystian"))
 print(check_palindrome("Zakaz"))
 print(check_palindrome("kajak"))
+print(check_palindrome(10))
